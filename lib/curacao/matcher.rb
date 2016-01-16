e6805b4a30adb5c083f46553520238dc61f1d3b0
pikesley <github@orgraphone.org>
2016-01-16 14:56:14 +0000

    Don't dick with STDOUT and STDERR
    If ENV[‘PRY’] is set

--- lib/curacao/matcher.rb
+++ lib/curacao/matcher.rb
@@ -16,16 +16,18 @@ RSpec.configure do |config|
   end
 
   # Suppress CLI output. This *will* break Pry
-  original_stderr = $stderr
-  original_stdout = $stdout
-  config.before(:all) do
-    # Redirect stderr and stdout
-    $stderr = File.new '/dev/null', 'w'
-    $stdout = File.new '/dev/null', 'w'
-  end
+  unless ENV['PRY']
+    original_stderr = $stderr
+    original_stdout = $stdout
+    config.before(:all) do
+      # Redirect stderr and stdout
+      $stderr = File.new '/dev/null', 'w'
+      $stdout = File.new '/dev/null', 'w'
+    end
 
-  config.after(:all) do
-    $stderr = original_stderr
-    $stdout = original_stdout
+    config.after(:all) do
+      $stderr = original_stderr
+      $stdout = original_stdout
+    end
   end
 end
